58cca51b62caf3321add67283eb87f2244d61d56
desync_lord <86848614+MarkGG8181@users.noreply.github.com>
2025-08-16 23:33:00 +0300

    feat: FastUse module

---
+++ src/main/java/today/vanta/client/module/impl/player/FastUse.java
@@ -0,0 +1,42 @@
+package today.vanta.client.module.impl.player;
+
+import today.vanta.client.event.impl.game.world.UpdateEvent;
+import today.vanta.client.module.Category;
+import today.vanta.client.module.Module;
+import today.vanta.client.setting.impl.NumberSetting;
+import today.vanta.util.game.events.EventListen;
+
+public class FastUse extends Module {
+    private final NumberSetting rightDelay = NumberSetting.builder()
+            .name("RMB delay")
+            .value(0)
+            .min(0)
+            .max(4)
+            .places(0)
+            .build(),
+
+    leftDelay = NumberSetting.builder()
+            .name("LMB delay")
+            .value(0)
+            .min(0)
+            .max(10)
+            .places(0)
+            .build();
+
+    public FastUse() {
+        super("FastUse", "Makes you use items faster", Category.PLAYER);
+        displayNames = new String[] {"FastUse", "Fast Use", "FastPlace", "Fast Place", "NoClickDelay", "No Click Delay"};
+    }
+
+    @EventListen
+    private void onUpdate(UpdateEvent event) {
+        mc.rightClickDelayTimer = rightDelay.getValue().intValue();
+        mc.leftClickCounter = leftDelay.getValue().intValue();
+    }
+
+    @Override
+    public void onDisable() {
+        mc.rightClickDelayTimer = 4;
+        mc.leftClickCounter = 10;
+    }
+}
